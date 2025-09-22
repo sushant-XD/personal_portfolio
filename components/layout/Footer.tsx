b6d21cb6ff8e85c76c40be0851f5d3d1df95f1c3
sushant-XD <sushantdhakal07@gmail.com>
2025-09-21 22:11:37 -0500

    better footer

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -3,7 +3,7 @@ export function Footer() {
     <footer className="py-10 text-center border-t border-gray-200 mt-24">
       <div className="container-minimal">
         <p className="text-xs opacity-50 tracking-wider">
-          &copy; Sushant Dhakal 2025 - Crafted with passion and precision
+          Copyright © Sushant Dhakal 2025
         </p>
       </div>
     </footer>
